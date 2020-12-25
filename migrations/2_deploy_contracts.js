761bbc0744d1b5f76b0dcff448e88818b3ca2910
mayank0508 <thakurmayank0508@gmail.com>
2020-12-25 21:06:29 +0530

    new

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,12 +2,4 @@ const Decentragram = artifacts.require("Decentragram");
 
 module.exports = function(deployer) {
   deployer.deploy(Decentragram);
-
-  // store posts
-
-  // Create post 
-
-  // tip post
-
-
 };
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
  string public name = "Decentragram";
+ 
+  // Storre Images
+mapping(uint => Image) public images;
+
+ struct Image {
+     uint id;
+     string hash;
+     string description;
+     uint tipAmount;
+     address payable author;
+ }
+ // Create images
+ function uploadImage() public {
+     images[1] = Image(1, 'abc123', 'Hello, world!', 0, address(0x0) );
+ }
+ // Tip Images
+
 }
