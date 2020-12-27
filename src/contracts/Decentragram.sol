71b615c9953ea052e487a7af2cea405816fefa46
mayank0508 <thakurmayank0508@gmail.com>
2020-12-27 14:54:11 +0530

    new

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -22,6 +22,19 @@ mapping(uint => Image) public images;
      uint tipAmount,
      address payable author
  );
+
+ event ImageTipped(
+     uint id,
+     string hash,
+     string description,
+     uint tipAmount,
+     address payable author
+ );
+
+  constructor() public {
+    name = "Decentragram";
+  }
+
  // Create images
  function uploadImage(string memory _imgHash, string memory _description) public {
     // Make sure that the image Hash exits
@@ -40,12 +53,29 @@ mapping(uint => Image) public images;
     imageCount ++;
         
      // Add image to contract
-     images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+     images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
 
      // Trigger on event
      emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
  }
 
  // Tip Images
+ function tipImageOwner(uint _id) public payable {
+// Make sure that the id is valid
+require(_id > 0 && _id <= imageCount);
+// Fetch Image
+Image memory _image = images[_id];
+// Fetch author
+address payable _author = _image.author;
+// Pay the autor by sendng them ether
+address(_author).transfer(msg.value);
+// Increment the tip amount
+_image.tipAmount = _image.tipAmount + msg.value;
+// Update the image
+images[_id] = _image;
+// Trigger a nd event
+emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+ }
+
 
 }
