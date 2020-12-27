0cd80bc1c4ce1b25a25a427d8d4f4ab50fe0bcd3
mayank0508 <thakurmayank0508@gmail.com>
2020-12-28 04:13:38 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,36 @@ import Main from './Main'
 
 class App extends Component {
 
+async componentWillMount() {
+  await this.loadWeb3()
+  await this.loadBlockchainData()
+}
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+async loadBlockchainData() {
+  const web3 = window.web3
+  // Load account
+  const accounts = await web3.eth.getAccounts()
+  this.setState({ account: accounts[0] })
+
+  const decentragram = web3.eth.Contract(abi, address)
+
+}
+
+  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -26,7 +56,6 @@ class App extends Component {
             // Code...
             />
           }
-        }
       </div>
     );
   }
